440225623cceae7b26c436335715a7517e196ce5
Gia1987 <giacomo.n27@gmail.com>
2018-08-10 16:58:29 +0100

    Create board test green

---
+++ lib/board.rb
@@ -0,0 +1,11 @@
+class Board
+
+  def draw_board(grid)
+    puts " #{grid[0]} | #{grid[1]} | #{grid[2]} "
+    puts "-----------"
+    puts " #{grid[3]} | #{grid[4]} | #{grid[5]} "
+    puts "-----------"
+    puts " #{grid[6]} | #{grid[7]} | #{grid[8]} "
+  end
+
+end
