a42b1ed639215d95e59d24b6af97203fa475b6ac
Gia1987 <giacomo.n27@gmail.com>
2018-08-11 08:33:40 +0100

    Naming refactored

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,10 @@
+# Class manage to draw the board in the console by reciving the game_state
 class Board
-
-  def draw_board(grid)
-    puts " #{grid[0]} | #{grid[1]} | #{grid[2]} "
-    puts "-----------"
-    puts " #{grid[3]} | #{grid[4]} | #{grid[5]} "
-    puts "-----------"
-    puts " #{grid[6]} | #{grid[7]} | #{grid[8]} "
+  def draw_board(game_state)
+    puts " #{game_state[0]} | #{game_state[1]} | #{game_state[2]} "
+    puts '-----------'
+    puts " #{game_state[3]} | #{game_state[4]} | #{game_state[5]} "
+    puts '-----------'
+    puts " #{game_state[6]} | #{game_state[7]} | #{game_state[8]} "
   end
-
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,8 +6,8 @@ describe Board do
 
   context '#draw_board' do
     it' returns array of nine elements' do
-      grid = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-      expect{board.draw_board(grid)}
+      game_state = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+      expect{board.draw_board(game_state)}
       .to output( "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
       .to_stdout
     end
