9cb2f66c2e4e35d845edbfef48cd955f0c403dac
Gia1987 <giacomo.n27@gmail.com>
2018-08-15 11:47:41 +0100

    Refactored code

--- lib/game.rb
+++ lib/game.rb
@@ -3,9 +3,15 @@ require_relative './board'
 
 # Game class manage all of the logic and state of the game.
 class Game
-  attr_reader :state
+  attr_reader :state, :current_player
 
   def initialize
     @state = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] # state of the game
+    @current_player = 'X'
+  end
+
+  def change_player
+    return @current_player = 'O' if @current_player == 'X'
+    @current_player = 'X'
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,4 +8,15 @@ describe Game do
       expect(game.state).to eq([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
     end
   end
+  context '#change_player' do
+    it 'turns current player from X to O' do
+      game.change_player
+      expect(game.current_player).to eq('O')
+    end
+    it 'turns current player from O to X' do
+      game.change_player
+      game.change_player
+      expect(game.current_player).to eq('X')
+    end
+  end
 end
