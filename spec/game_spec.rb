f2a65176302254ee5d931c055c8e57e142c324fc
Gia1987 <giacomo.n27@gmail.com>
2018-08-15 14:39:12 +0100

    Winning conditions test red

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -31,4 +31,52 @@ describe Game do
       expect(game.state).to eq(['X', ' ', 'O', ' ', 'X', ' ', 'O', ' ', ' '])
     end
   end
+  context '#horizontal_winning' do
+    it 'should check if there is a winner on the row' do
+      game.play(0)
+      game.change_player
+      game.play(5)
+      game.change_player
+      game.play(1)
+      game.change_player
+      game.play(3)
+      game.change_player
+      game.play(2)
+      expect(game.horizontal).to eq(true)
+    end
+  end
+  context '#vertical_winning' do
+    it 'should check if there is a winner on the column' do
+      game.play(0)
+      game.change_player
+      game.play(4)
+      game.change_player
+      game.play(3)
+      game.change_player
+      game.play(2)
+      game.change_player
+      game.play(6)
+      expect(game.vertical).to eq(true)
+    end
+  end
+  context '#diagonal_winning' do
+    it 'should check if there is a winner on the diagonal' do
+      game.play(0)
+      game.change_player
+      game.play(5)
+      game.change_player
+      game.play(4)
+      game.change_player
+      game.play(3)
+      game.change_player
+      game.play(8)
+      expect(game.diagonal).to eq(true)
+    end
+  end
+  context '#won?' do
+    it 'should check if there is a winner' do
+      allow(game).to receive(:horizontal) { true }
+      expect(game.won?).to eq(true)
+    end
+  end
 end
