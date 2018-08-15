67ab0712177d75c75360393d5c2a4feb22b6c713
Gia1987 <giacomo.n27@gmail.com>
2018-08-15 12:30:54 +0100

    create method play. test red

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,4 +19,16 @@ describe Game do
       expect(game.current_player).to eq('X')
     end
   end
+  context '#play' do
+    it 'should update the state of the game' do
+      game.play(0)
+      game.change_player
+      game.play(2)
+      game.change_player
+      game.play(4)
+      game.change_player
+      game.play(6)
+      expect(game.state).to eq(['X', ' ', 'O', ' ', 'X', ' ', 'O', ' ', ' '])
+    end
+  end
 end
