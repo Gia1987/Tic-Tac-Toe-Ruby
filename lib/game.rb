9e9b437bb1ed649e3a538b158a483e9881246f0c
Gia1987 <giacomo.n27@gmail.com>
2018-08-23 10:41:42 +0100

    Refactored code game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,44 +3,34 @@ require_relative './board'
 
 # Game class manage all of the logic and state of the game.
 class Game
-  attr_reader :state, :current_player
+  attr_reader :count, :over, :players
 
-  def initialize
-    @state = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] # state of the game
-    @current_player = 'X'
+  def initialize(human = Human.new, board = Board.new)
+    # state of the game
+    @human = human
+    @board = board
+    @players = ['O', 'X']
+    @count = -1
+  end
+
+  def winning_combinations
+    [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ]
+  end
+
+  def current_player
+    @players.first
   end
 
   def change_player
-    return @current_player = 'O' if @current_player == 'X'
-    @current_player = 'X'
+    @count += 1
+    @players.rotate!
   end
 
-  def play(input)
-    @state[input] = @current_player
+  def won?(player = current_player)
+    winning_combinations.any? { |combo| combo.all? { |index| @board.grid[index] == player} }
   end
-
-  def horizontal
-    true if
-      @state[0] == @current_player && @state[1] == @current_player && @state[2] == @current_player ||
-      @state[3] == @current_player && @state[4] == @current_player && @state[5] == @current_player ||
-      @state[6] == @current_player && @state[7] == @current_player && @state[8] == @current_player
-  end
-
-  def vertical
-    true if
-      @state[0] == @current_player && @state[3] == @current_player && @state[6] == @current_player ||
-      @state[1] == @current_player && @state[4] == @current_player && @state[7] == @current_player ||
-      @state[2] == @current_player && @state[5] == @current_player && @state[8] == @current_player
-  end
-
-  def diagonal
-    true if
-      @state[0] == @current_player && @state[4] == @current_player && @state[8] == @current_player ||
-      @state[2] == @current_player && @state[4] == @current_player && @state[6] == @current_player
-  end
-
-  def won?
-    true if horizontal == true || vertical == true || diagonal == true
-  end
-
 end
