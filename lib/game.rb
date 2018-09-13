57c985f5c311f0f81cce72963a62a40af937b2f6
Gia1987 <giacomo.n27@gmail.com>
2018-09-13 14:08:20 +0100

    Refactored game class code

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,17 @@
 require_relative './human'
 require_relative './board'
+require_relative './Ai'
 
 # Game class manage all of the logic and state of the game.
 class Game
-  attr_reader :count, :over, :players, :game_over
+  attr_reader :count, :over, :players
 
-  def initialize(human = Human.new, board = Board.new)
-    # state of the game
-    @human = human
-    @board = board
-    @players = ['O', 'X']
+  def initialize(human_class = Human, board_class = Board, computer_class = AI)
+    @computer_class = computer_class
+    @human_class = human_class
+    @board = board_class.new
+    @players = []
     @count = -1
-    @game_over = false
   end
 
   def winning_combinations
@@ -22,50 +22,90 @@ class Game
     ]
   end
 
-  def play
-    until game_over
-      if over?
-        @board.draw_board
-        @game_over = true
-        return no_wins if draw?
-        return congrats if won?(current_player) || won?(@players.last)
-      else
-        @board.draw_board
-        change_player
-        move = @human.move
-        @board.update_grid(move, current_player)
-      end
+  def welcome
+    puts 'Welcome to Tic Tac Toe. Please chose the game mode:'
+    puts 'press 1 for human VS human'
+    puts 'press 2 for computer VS computer'
+    puts 'press 3 for human VS computer'
+  end
+
+  def menu
+    welcome
+    input = gets.to_i
+    case input
+    when 1 then @players = [@human_class.new('X'), @human_class.new('O')]
+    when 2 then @players = [@computer_class.new('X'), @computer_class.new('O')]
+    when 3 then @players = [@human_class.new('X'), @computer_class.new('O')]
     end
+    start
+    play_again?
+  end
+
+  def play_again?
+    puts 'play again? y/n'
+    again = gets.chomp
+    again == 'y' ? reset_game : exit
+  end
+
+  def take_turn
+    @board.draw_board
+    move = current_player.move(@board)
+    change_player if @board.update_grid(move, current_player.mark)
+  end
+
+  def start
+    take_turn until game_over?
+    result
+  end
+
+  def result
+    @players.rotate!
+    @board.draw_board
+    return no_wins if draw?
+    return congrats if won?
+  end
+
+  def reset_game
+    @board.reset_grid
+    @count = -1
+    menu
   end
 
   def current_player
     @players.first
   end
 
-  def change_player
+  def increase_count
     @count += 1
-    @players.rotate!
   end
 
-  def won?(player = current_player)
-    # should check if any combination has neen matched during the game.
-    winning_combinations.any? { |combo| combo.all? { |index| @board.grid[index] == player} }
+  def change_player
+    @players.rotate!
+    increase_count
+    puts "Player #{current_player.mark} is your turn."
+  end
+  # should check if any combination has neen matched during the game.
+  def won_by?(player)
+    winning_combinations.any? { |combo| combo.all? { |index| @board.grid[index] == player.mark} }
+  end
+
+  def won?
+    won_by?(@players.first) || won_by?(@players.last)
   end
 
   def draw?
-    !won?(current_player) && !won?(@players.last) && count == 8
+    !won? && count == 8
   end
 
   def congrats
-    puts "Congratulations #{current_player}"
+    puts "Congratulations #{current_player.mark}"
   end
 
   def no_wins
     puts 'Its a Draw!'
   end
-
-  def over?
-    # should check if the game either won or draw.
-    won?(current_player) || won?(@players.last) || draw?
+  # should check if the game either won or draw.
+  def game_over?
+    won? || draw?
   end
 end
