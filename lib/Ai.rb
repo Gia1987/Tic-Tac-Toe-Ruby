94592842b942eeebe9e248a09a1d3338d8724c04
Gia1987 <giacomo.n27@gmail.com>
2018-09-14 15:22:26 +0100

    Created negamax unbeatable algorithm

--- lib/Ai.rb
+++ lib/Ai.rb
@@ -10,7 +10,33 @@ class AI
     true
   end
 
-  def move(board)
-    board.available_indexes.sample
+  def move(game)
+    sleep(1)
+    negamax(game, 0, {})
   end
+
+private
+
+  def heuristc_value(game)
+    game.won? ? -1 : 0
+  end
+
+  def negamax(game, depth, scores_hash)
+    if game.game_over?
+      return heuristc_value(game)
+    else
+      game.board.available_indexes.each do |index|
+        game.board.update_grid(index, game.current_player)
+        scores_hash[index] = (-1 * negamax(game, depth + 1, {}))
+        game.board.reset_tile(index)
+      end
+      if depth.zero?
+        return scores_hash.sort_by { |_key, value| value }.last.first
+      else
+        return scores_hash.sort_by { |_key, value| value }.last.last
+      end
+    end
+  end
+
+
 end
