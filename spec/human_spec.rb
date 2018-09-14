1118a1525911cb2ec12cddcac4819ef8a22b9277
Gia1987 <giacomo.n27@gmail.com>
2018-09-14 15:15:24 +0100

    Refactored test human class. green

--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -2,19 +2,24 @@ require 'spec_helper'
 require './lib/human'
 
 describe Human do
-  subject(:human) { Human.new }
-  context 'player can chose the mark' do
-    it 'change mark attribute' do
-      allow(human).to receive(:gets) { 'X' }
-      human.human_mark
-      expect(human.mark).to eq('X')
+  subject(:human) { Human.new('X') }
+
+  # context 'player can chose the mark' do
+  #   it 'change mark attribute' do
+  #     allow(human).to receive(:gets) { 'X' }
+  #     human.human_mark
+  #     expect(human.mark).to eq('X')
+  #   end
+  # end
+  context '#is_computer?' do
+    it 'returns false' do
+      expect(human.is_computer?).to eq(false)
     end
   end
   context '#move' do
     it 'recives input from player' do
       allow(human).to receive(:gets) { 1 }
-      expect(human.move).to eq(1)
-      expect { human.move }.to output("Please insert a number between 0-8\n").to_stdout
+      expect(human.move(:game)).to eq(1)
     end
   end
 end
